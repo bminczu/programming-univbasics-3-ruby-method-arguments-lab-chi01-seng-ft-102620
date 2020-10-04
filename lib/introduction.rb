64567e7095cb84fc718152c3e79e15a5caee728b
adventurous-format-6947 <bminczu@gmail.com>
2020-10-04 17:28:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction("Bart")
+introduction("Bart","Ruby")
 
 
 
